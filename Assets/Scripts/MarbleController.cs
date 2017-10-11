34448be4479bd41933560749c6690ac777dae9a3
Abbondanzo <peter@abbondanzo.com>
2017-10-11 17:32:38 -0400

    Endgame

---
+++ Assets/Scripts/MarbleController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleController : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Finish") {
+			print ("Game over");
+			GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+			Application.Quit ();
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,14 +10,20 @@ public class PlayerController : MonoBehaviour {
 	public MeshCollider ground;
 
 	private Rigidbody body;
+	private bool gameOver;
 
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody> ();
+		gameOver = false;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (gameOver) {
+			return;
+		}
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical") * -1;
 
@@ -32,10 +38,21 @@ public class PlayerController : MonoBehaviour {
 		CheckOver ();
 	}
 
+	// Check if the marble has fallen below the game floor
 	void CheckOver () {
 		float lowest = Mathf.Infinity;
 		if (marble.transform.position.y < ground.bounds.min.y) {
-			print ("DONE: " + marble.transform.position.y + ", " + ground.bounds.min.y);
+			print ("Fallen from world!");
+			FreezeAll ();
 		}
+	}
+
+	// Freeze all motion and input movements
+	void FreezeAll() {
+		gameOver = true;
+		body.constraints = RigidbodyConstraints.FreezeAll;
+		Rigidbody rigidMarble = marble.GetComponent<Rigidbody>();
+		rigidMarble.constraints = RigidbodyConstraints.FreezeAll;
+		Application.Quit ();
 	}
 }
